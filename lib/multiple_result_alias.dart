2d601e3c9b591f530aa1ab6e6766f01e2422a714
Gustavo de Oliveira Silva (DIG) <contato@dig.dev.br>
2025-05-16 12:09:56 -0300

    Added dartdoc

--- lib/multiple_result_alias.dart
+++ lib/multiple_result_alias.dart
@@ -1,10 +1,13 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
+/// Provide aliases for Result classes
 library;
 
 import 'package:multiple_result/multiple_result.dart';
 
+/// Alias for Result
 typedef $Result<S, E> = Result<S, E>;
+
+/// Alias for Success
 typedef $Success<S, E> = Success<S, E>;
+
+/// Alias for Error
 typedef $Error<S, E> = Error<S, E>;
